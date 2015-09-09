51a698ea9ca2d9f61416dcb2df0d44a52e5107ca
Christian Neudert <christian.neudert@epost-dev.de>
2015-09-09 09:27:02 +0200

    Add basic config.
    * Copied from puppet-os-hardening.
    * Base hardening on garethr/docker.

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## Todos to get up and testing
+* Setup spec_helper etc.
+* Start with a simple change
+* Check if kitchen-docker could be helpful for faster testing.
+* Add support for configuration parameters as given by tests-hardening
---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+# == Class: docker_hardening
+#
+# Configures overlay hardening
+#
+# === Parameters
+#
+# [*provider*]
+#   The name of the provider you use to install docker.
+#   Supported: `garethr/docker`
+#
+class docker_hardening(
+  $provider = 'none',
+) {
+  case $provider {
+    'garethr/docker': {
+      class{'docker_hardening::garethr': }
+    }
+    'none': {
+      fail('You haven\'t configured a Docker provider for hardening.')
+    }
+    default: {
+      fail('Unrecognized/Unsupported Docker provider for hardening.')
+    }
+  }
+}
